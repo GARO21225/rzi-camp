e84a6a22fdc0dbb605abe737c1f308fe7b88611a
GARO21225 <edgar.kouame@orange.com>
2026-05-16 12:29:45 +0000

    okok

--- backend/restauration/urls.py
+++ backend/restauration/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from .views import QRTokenViewSet, RepasLogViewSet, AuditLogViewSet
 router = DefaultRouter()
-router.register('qr', QRTokenViewSet)
+router.register('qr', QRTokenViewSet, basename='qr')
 router.register('repas', RepasLogViewSet)
 router.register('audit', AuditLogViewSet)
 urlpatterns = router.urls
