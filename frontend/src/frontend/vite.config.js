061051fa9159b04cbd1b9002ae9b135fea5dcbb7
GARO21225 <edgar.kouame@orange.com>
2026-05-30 14:21:17 +0000

    OK

--- frontend/src/frontend/vite.config.js
+++ frontend/src/frontend/vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
     rollupOptions: {
+      input: './index.html',
       output: {
         manualChunks: {
-          react: ['react','react-dom'],
-          leaflet: ['leaflet','react-leaflet'],
+          react: ['react', 'react-dom'],
+          leaflet: ['leaflet', 'react-leaflet'],
           charts: ['recharts'],
         },
       },
