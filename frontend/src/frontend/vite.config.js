b01a24cb2f317b5566cdd5d1cb8ea58ecf64fdd2
GARO21225 <edgar.kouame@orange.com>
2026-05-30 14:32:25 +0000

    OKOK

--- frontend/src/frontend/vite.config.js
+++ frontend/src/frontend/vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: './index.html',
       output: {
         manualChunks: {
           react: ['react', 'react-dom'],
