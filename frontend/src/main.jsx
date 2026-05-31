6967f2a7b9868888a798899589cc6ca1b43fa448
GARO21225 <edgar.kouame@orange.com>
2026-05-31 11:55:14 +0000

    feat: frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,13 @@ import App from './App'
 import './index.css'
 import './theme.css'
 
+// Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {})
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
