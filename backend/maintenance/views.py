ceed68ab7a203d37af103873c5d2945762b1b15a
GARO21225 <edgar.kouame@orange.com>
2026-06-20 01:41:17 +0000

    CRITICAL FIX: IsAuthenticated was never imported, causing NameError crashing entire urls.py load (breaking login + ALL endpoints)

--- backend/maintenance/views.py
+++ backend/maintenance/views.py
@@ -35,7 +35,7 @@ def _notifier(incident, message, type_notif='info'):
 
 
 from rest_framework.decorators import api_view, permission_classes as pc
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 @api_view(['GET'])
 @pc([IsAuthenticated])
