dfa5dadf9f3db0321623957ea84c3cfb48e8d2b2
GARO21225 <edgar.kouame@orange.com>
2026-06-20 08:30:56 +0000

    feat: register 4 new routes for editable Induction Camp content (config/infras/regles/quiz)

--- backend/residences/urls.py
+++ backend/residences/urls.py
@@ -1,6 +1,8 @@
 
 from rest_framework.routers import DefaultRouter
-from .views import InductionRecordViewSet, declarer_soustraitants_masse, BatimentViewSet, PersonnelViewSet, OccupationHistoryViewSet, OccupationHistoryAdminViewSet, DemandeViewSet
+from .views import (InductionRecordViewSet, declarer_soustraitants_masse, BatimentViewSet,
+    PersonnelViewSet, OccupationHistoryViewSet, OccupationHistoryAdminViewSet, DemandeViewSet,
+    InductionCampConfigViewSet, InductionInfraViewSet, InductionRegleViewSet, InductionQuizQuestionViewSet)
 router = DefaultRouter()
 router.register(r'induction-records', InductionRecordViewSet, basename='induction-record')
 router.register("batiments", BatimentViewSet)
@@ -8,5 +10,9 @@ router.register("personnel", PersonnelViewSet)
 router.register("occupation-history", OccupationHistoryViewSet)
 router.register("occupation-history-admin", OccupationHistoryAdminViewSet, basename="occupation-history-admin")
 router.register("demandes", DemandeViewSet, basename="demandes")
+router.register("induction-config", InductionCampConfigViewSet, basename="induction-config")
+router.register("induction-infras", InductionInfraViewSet, basename="induction-infra")
+router.register("induction-regles", InductionRegleViewSet, basename="induction-regle")
+router.register("induction-quiz", InductionQuizQuestionViewSet, basename="induction-quiz")
 from django.urls import path
 urlpatterns = list(router.urls) + [path("declarer-soustraitants/", declarer_soustraitants_masse)]
